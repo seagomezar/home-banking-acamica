52c5dddf8e9e7fa157004e10bed9a5babb50a01d
francoguffanti <franco_guffanti@hotmail.com>
2020-08-31 23:27:58 -0300

    creacion de tabla cuentas

--- sql/creacion_tablas.sql
+++ sql/creacion_tablas.sql
@@ -3,6 +3,13 @@
 
 // Fran cuentas
 
+CREATE TABLE cuentas(
+   cuenta_id smallint(5) unsigned not null auto_increment primary key,
+   nombre varchar(50) not null,
+   tipo varchar(50) not null,
+   saldo INT not null
+);
+
 // rodri usuarios
 
 // transacciones Miguel
