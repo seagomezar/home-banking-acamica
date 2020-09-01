f0faa014123888742e184c4fd66e00af605b7ad3
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2020-08-31 20:23:31 -0500

    Archivo inicial

---
+++ server.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const server = express();
+const bodyParser = require("body-parser");
+const cuentas = [
+    {titular:"martin",nroCuenta:"0123123123",saldo:"14566"},
+    {titular:"franco",nroCuenta:"9999999999",saldo:"99999"},
+    {titular:"manuel",nroCuenta:"5555555555",saldo:"55555"}
+];
+
+server.listen(3000, ()=>{});
+
+server.use(bodyParser.json());
+
+server.get("/cuentas", (req,res,err)=>{
+    res.json(cuentas);
+});
