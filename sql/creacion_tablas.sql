d7859c56d3595f41561cc1fac55a31dce9b8fc3e
francoguffanti <franco_guffanti@hotmail.com>
2020-08-31 23:37:38 -0300

    creacion de tabla cuentas

--- sql/creacion_tablas.sql
+++ sql/creacion_tablas.sql
@@ -1,7 +1,7 @@
-// TODO: Escribir el codigo para crear tres tablas cuentas, usuarios, transacciones
+-- TODO: Escribir el codigo para crear tres tablas cuentas, usuarios, transacciones
 
 
-// Fran cuentas
+--Fran cuentas
 
 CREATE TABLE cuentas(
    cuenta_id smallint(5)  not null auto_increment primary key,
@@ -10,6 +10,6 @@ CREATE TABLE cuentas(
    saldo INT(10) not null
 );
 
-// rodri usuarios
+-- rodri usuarios
 
-// transacciones Miguel
+--transacciones Miguel
