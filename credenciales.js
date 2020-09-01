5d036edea69fa1a83fb914d290186dd186590b71
Sebastian Alonso Gomez Arias <seagomezar@gmail.com>
2020-08-31 21:41:32 -0500

    Añadiendo credenciales

---
+++ credenciales.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // pedir password a Sebas
+    servidor:
+        {
+            desarrollo: "mysql://sebas:<password>@192.168.64.2:3306/clase_sql",
+            local: "mysql://sebas:<password>@192.168.64.2:3306/local"
+        }
+}
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const server = express();
 const bodyParser = require("body-parser");
+const credenciales = require('./credenciales');
 const cuentas = [
     {titular:"martin",nroCuenta:"0123123123",saldo:"0"},
     {titular:"franco",nroCuenta:"9999999999",saldo:"99999"},
     {titular:"manuel",nroCuenta:"5555555555",saldo:"55555"}
 ];
+const urls = require(credenciales)
 
 // Añadiendo la linea de conexion
 const conexion = new sequelizer(
-    "mysql://sebas:some_pass@192.168.64.2:3306/clase_sql"
+    urls.server.desarrollo
   );
 
 
