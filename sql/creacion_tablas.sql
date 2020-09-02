e86fbdfa599873b0714dfc467a7b08fbec977730
Juan Manuel Rosello <jmrosello@gmail.com>
2020-09-02 16:21:38 -0300

    corrección de CREATE TABLE usuarios

--- sql/creacion_tablas.sql
+++ sql/creacion_tablas.sql
@@ -11,5 +11,13 @@ CREATE TABLE cuentas(
 );
 
 -- rodri usuarios
+CREATE TABLE usuarios (
+    id smallint(5) auto_increment primary key,
+    user varchar(20) not null,
+    pass varchar(40) not null,
+    nombre varchar(60) not null,
+    apellido varchar(60) not null,
+    email varchar(40) not null
+)
 
 --transacciones Miguel
